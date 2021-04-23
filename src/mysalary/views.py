ae3825a8b88b8a72cac21809122fe2c8b7911c80
Jan Dale Amores <amoresjan14@gmail.com>
2021-04-23 23:22:35 +0800

    added views for app mysalary

--- src/mysalary/views.py
+++ src/mysalary/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+
+from src.mysalary import models, serializers
 
 # Create your views here.
+
+
+class CompanyViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = models.Company.objects
+    serializer_class = serializers.CompanySerializer
+
+
+class LevelViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = models.Level.objects
+    serializer_class = serializers.LevelSerializer
+
+
+class CompensationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = models.Compensation.objects
+    serializer_class = serializers.CompensationSerializer
+
+
+class ContributionViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = models.Contribution.objects
+    serializer_class = serializers.ContributionSerializer
+
+
+class CeritificateViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = models.Certificate.objects
+    serializer_class = serializers.CertificateSerializer
